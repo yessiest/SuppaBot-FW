16b3a534b1c570f5c201b661c2171b77086629cf
Yessiest <yessiest@protonmail.com>
2020-11-18 01:54:06 +0400

    Added prefix and command name purification

--- libraries/classes/command-handler.lua
+++ libraries/classes/command-handler.lua
@@ -6,27 +6,36 @@
 --plugin handlers are unknown.
 local class = import("classes.baseclass")
 local command_handler = class("Command-handler")
+local table_utils = import("table-utils")
+local purify = import("purify")
 function command_handler:__init(parent_server)
   self.server_handler = assert(parent_server,"parent server handler not provided")
   self.command_pool = {}
   self.prefixes = {}
 end
 function command_handler:add_prefix(prefix)
-  self.prefixes[prefix] = prefix
+  local purified_prefix = purify.purify_escapes(prefix)
+  self.prefixes[purified_prefix] = purified_prefix
 end
 function command_handler:remove_prefix(prefix)
-  if self.prefixes[prefix] then
-    self.prefix[prefix] = nil
+  local purified_prefix = purify.purify_escapes(prefix)
+  if self.prefixes[purified_prefix] then
+    self.prefix[purified_prefix] = nil
   end
+end
+function command_handler:get_prefixes()
+  return table_utils.deepcopy(self.prefixes)
 end
 function command_handler:add_command(command)
   assert(type(command) == "table","command object expected")
-  self.command_pool[command.name] = command
+  local purified_name = purify.purify_escapes(command.name)
+  self.command_pool[purified_name] = command
 end
 function command_handler:remove_command(command)
   assert(type(command) == "table","command object expected")
-  if self.command_pool[command.name] then
-    self.command_pool[command.name] = nil
+  local purified_name = purify.purify_escapes(command.name)
+  if self.command_pool[purified_name] then
+    self.command_pool[purified_name] = nil
   end
 end
 function command_handler:handle(message)
@@ -39,13 +48,13 @@ function command_handler:handle(message)
     else
       if command.options.prefix then
         for _,prefix in pairs(self.prefixes) do
-          if message.content:find(prefix..command.name,1,true) == 1 then
+          if message.content:find(prefix..name.."$") == 1 or message.content:find(prefix..name.."%s") then
             command:exec(message)
             return
           end
         end
       else
-        if message.content:find(command.name,1,true) == 1 then
+        if message.content:find(name.."$") == 1 or message.content:find(name.."%s") then
           command:exec(message)
           return
         end
